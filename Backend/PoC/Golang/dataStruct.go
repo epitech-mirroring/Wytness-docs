1d35f1b49b5587df390c8f2fd05f870dcecc5889
6im0n <simon.gl3135@orange.fr>
2024-11-27 19:18:28 +0100

    :sparkles: Add simple GIN PoC in Go

---
+++ Backend/PoC/Golang/dataStruct.go
@@ -0,0 +1,9 @@
+package main
+
+var books = []Book{
+	{ID: "1", Title: "The Lord of the Rings", Author: "J.R.R. Tolkien", Genre: Fantasy, Year: 1954, Pages: 1178},
+	{ID: "2", Title: "Harry Potter and the Philosopher's Stone", Author: "J.K. Rowling", Genre: Fantasy, Year: 1997, Pages: 223},
+	{ID: "3", Title: "The Da Vinci Code", Author: "Dan Brown", Genre: Thriller, Year: 2003, Pages: 689},
+	{ID: "4", Title: "The Alchemist", Author: "Paulo Coelho", Genre: Drama, Year: 1988, Pages: 177},
+	{ID: "5", Title: "The Catcher in the Rye", Author: "J.D. Salinger", Genre: Drama, Year: 1951, Pages: 277},
+}
