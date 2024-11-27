28d16d4f4aeba898c81a00cc5182e12111e99b94
Antoinegtir <antoine.gonthier@epitech.eu>
2024-11-27 11:09:52 +0100

    ✨create api object that contain data types like name, actions..

---
+++ Mobile/PoC/flutter/lib/model/apis.module.dart
@@ -0,0 +1,13 @@
+class ApiModel {
+  final String name;
+  final String imageUrl;
+  final String description;
+  final List<String> actions;
+
+  ApiModel({
+    required this.name,
+    required this.imageUrl,
+    required this.description,
+    required this.actions,
+  });
+}
