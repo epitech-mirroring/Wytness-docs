59ef9b693b84a98f509b0ee037353815c4a7b13f
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-11-27 10:12:00 +0100

    ✨ Add Navbar and Footer in the Angular PoC

--- Frontend/PoC/Angular/src/app/app.component.spec.ts
+++ Frontend/PoC/Angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular' title`, () => {
+  it(`should have the 'poc-front-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('poc-front-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, poc-front-angular');
   });
 });
