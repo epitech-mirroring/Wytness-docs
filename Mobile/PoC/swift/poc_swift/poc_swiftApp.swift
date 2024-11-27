4db8b95a75f0460e109bece26af557001c44896c
Antoinegtir <antoine.gonthier@epitech.eu>
2024-11-27 11:46:14 +0100

    ✨ create the swift poc

---
+++ Mobile/PoC/swift/poc_swift/poc_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_swiftApp.swift
+//  poc_swift
+//
+//  Created by Antoine on 26/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct poc_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mobile/PoC/swift/poc_swiftTests/poc_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  poc_swiftTests.swift
+//  poc_swiftTests
+//
+//  Created by Antoine on 26/11/2024.
+//
+
+import Testing
+@testable import poc_swift
+
+struct poc_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
