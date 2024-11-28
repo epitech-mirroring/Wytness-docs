9a9e569c3510997ba4fdc8b2fa2e7d63d955d88c
Axel Eckenberg <axel.eckenberg@yahoo.fr>
2024-11-28 11:03:20 +0100

    ✨ Add PoC for Next/React

---
+++ Web/PoC/Next/app/rick/page.tsx
@@ -0,0 +1,12 @@
+export default function Rick() {
+  return (
+      <div className='flex justify-center items-center w-screen h-screen'>
+          <iframe width="560" height="315"
+                  src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=Sy8hDEEBg-Pcoxk7&amp;controls=0&amp;autoplay=1&amp;loop=1"
+                  title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen></iframe>
+      </div>
+  );
+}
